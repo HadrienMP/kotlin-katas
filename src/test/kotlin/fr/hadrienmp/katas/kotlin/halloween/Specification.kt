0675c95a1a0c2e16f1751cd9261b50c80bcbf8e0
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-10-28 14:04:30 +0100

    Kids Are not satisfied when they get a different amount of candy - Refactor

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/halloween/Specification.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/halloween/Specification.kt
@@ -39,9 +39,9 @@ fun trickOrTreat(i: Int, candyDistribution: CandyDistribution) = when {
 
 class CandyDistribution(private val candyDistribution: List<List<String>>) {
     fun isSatisfactory(): Boolean {
-        return (candyDistribution[0].size != candyDistribution[1].size)
-                || (candyDistribution.size == 4 && candyDistribution[0].size != candyDistribution[3].size)
+        return (candyDistribution.size == 2 && candyDistribution[0].size != candyDistribution[1].size)
                 || (candyDistribution.size == 3 && candyDistribution[0].size != candyDistribution[2].size)
+                || (candyDistribution.size == 4 && candyDistribution[0].size != candyDistribution[3].size)
     }
 
 }
