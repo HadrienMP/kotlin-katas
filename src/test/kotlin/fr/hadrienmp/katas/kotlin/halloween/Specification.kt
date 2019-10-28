b2b289351c2cc42b533769d34f325fbb0bbf2e57
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-10-28 13:38:24 +0100

    Kids receiving the same amount of candy are satisfied

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/halloween/Specification.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/halloween/Specification.kt
@@ -1,12 +1,15 @@
 package fr.hadrienmp.katas.kotlin.halloween
 
+import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 object Specification: Spek({
     describe("Kids response") {
         it("A single kid is always satisfied") {
-
+            assertThat(trickOrTreat(2, listOf(listOf("candy"), listOf("candy")))).isEqualTo("Thank you, strange uncle!")
         }
     }
 })
+
+fun trickOrTreat(i: Int, listOf: List<List<String>>) = "Thank you, strange uncle!"
