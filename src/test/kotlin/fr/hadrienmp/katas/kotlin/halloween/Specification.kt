6abdb136a5710a9739d8c60b87eabcfeb07a8cbf
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-10-28 13:32:19 +0100

    Start spec

---
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/halloween/Specification.kt
@@ -0,0 +1,12 @@
+package fr.hadrienmp.katas.kotlin.halloween
+
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object Specification: Spek({
+    describe("Kids") {
+        it("") {
+
+        }
+    }
+})
