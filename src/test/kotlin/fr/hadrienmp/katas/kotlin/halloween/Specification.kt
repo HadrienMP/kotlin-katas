07e4111b743348af1b1b629641da8876a1a43e1e
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-10-28 13:35:21 +0100

    Add assertj

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/halloween/Specification.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/halloween/Specification.kt
@@ -4,7 +4,7 @@ import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 object Specification: Spek({
-    describe("Kids statisfaction") {
+    describe("Kids response") {
         it("A single kid is always satisfied") {
 
         }
