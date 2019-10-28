1c3773a363a210a48ef72e7d159d459e13784f00
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-10-28 13:47:41 +0100

    Kids Are not satisfied when they get a different amount of candy - add an example

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/halloween/Specification.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/halloween/Specification.kt
@@ -11,7 +11,8 @@ object Specification : Spek({
             assertThat(response).isEqualTo("Thank you, strange uncle!")
         }
         listOf(
-            listOf(listOf("candy", "candy"), listOf("candy"))
+            listOf(listOf("candy", "candy"), listOf("candy")),
+            listOf(listOf("candy"), listOf("candy", "candy"))
         ).forEach { candyDistribution ->
             it("Are not satisfied when they don't get the same number of candy - $candyDistribution") {
                 val response = trickOrTreat(2, candyDistribution)
@@ -23,5 +24,6 @@ object Specification : Spek({
 
 fun trickOrTreat(i: Int, listOf: List<List<String>>) = when {
     listOf[0].size > listOf[1].size -> "Trick or treat!"
+    listOf[1].size > listOf[0].size -> "Trick or treat!"
     else -> "Thank you, strange uncle!"
 }
