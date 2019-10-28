4d1662e375daa4b60dc4a3d6380653d999f1eb4c
Hadrien Mens-Pellen <hmens@financeactive.com>
2019-10-28 13:39:06 +0100

    Kids receiving the same amount of candy are satisfied - Refactor the test names

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/halloween/Specification.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/halloween/Specification.kt
@@ -5,8 +5,8 @@ import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 object Specification: Spek({
-    describe("Kids response") {
-        it("A single kid is always satisfied") {
+    describe("Kids") {
+        it("Are satisfied when they get the same amount of candy") {
             assertThat(trickOrTreat(2, listOf(listOf("candy"), listOf("candy")))).isEqualTo("Thank you, strange uncle!")
         }
     }
