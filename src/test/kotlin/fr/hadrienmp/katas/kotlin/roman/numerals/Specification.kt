95a598fab06ab0639dbf2d14934340ea12cfe4fd
Thomas Carpaye <tcarpaye@financeactive.com>
2020-03-25 10:13:38 +0100

    In Roman numeral : - 1 is 'I' - 2 is 'II'

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/roman/numerals/Specification.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/roman/numerals/Specification.kt
@@ -1,7 +1,23 @@
 package fr.hadrienmp.katas.kotlin.roman.numerals
 
+import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 
 object Specification: Spek({
 
+    describe("In Roman numeral ") {
+        it("1 is 'I'") {
+            assertThat(toRoman(1)).isEqualTo("I")
+        }
+        it("2 is 'II'") {
+            assertThat(toRoman(2)).isEqualTo("II")
+        }
+    }
+
 })
+
+fun toRoman(arabicNumber: Int) = when(arabicNumber) {
+    2 -> "II"
+    else -> "I"
+}
