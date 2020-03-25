90d4be238d8be17e24683f575203733e0ee73733
HadrienMP <github@hadrienmp.fr>
2020-03-25 10:33:27 +0100

    In Roman numeral : - 50 is 'L'

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/roman/numerals/Specification.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/roman/numerals/Specification.kt
@@ -13,7 +13,8 @@ object Specification : Spek({
             Pair(3, "III"),
             Pair(5, "V"),
             Pair(6, "VI"),
-            Pair(10, "X")
+            Pair(10, "X"),
+            Pair(50, "L")
         ).forEach { (arabic, roman) ->
             it("$arabic is '$roman'") {
                 assertThat(toRoman(arabic)).isEqualTo(roman)
@@ -26,4 +27,5 @@ fun toRoman(arabicNumber: Int): String {
     return "I".repeat(arabicNumber)
         .replace("IIIII", "V")
         .replace("VV", "X")
+        .replace("XXXXX", "L")
 }
