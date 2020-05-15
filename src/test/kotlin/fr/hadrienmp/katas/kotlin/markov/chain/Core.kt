4a6cd831da48e59fd84338ed6a67bc85273761a9
Thomas Carpaye <tcarpaye@financeactive.com>
2020-05-15 15:55:40 +0200

    "mob next [ci-skip]"

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/markov/chain/Core.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/markov/chain/Core.kt
@@ -6,34 +6,32 @@ import org.spekframework.spek2.style.specification.describe
 import java.util.*
 
 object Core : Spek({
-    describe("Hihi") {
+    describe("Generated text") {
         it("one word") {
             assertThat(markov(1, "coucou", Random())).isEqualTo("coucou")
         }
         it("another single word word") {
             assertThat(markov(1, "salut", Random())).isEqualTo("salut")
         }
-        it("two words") {
+        it("is the first word for a single sentence") {
             assertThat(markov(1, "salut toi", Random())).isEqualTo("salut")
         }
 
-        (0..9000).forEach {
-            it("two words - $it") {
-                assertThat(markov(1, "Salut toi. Coucou Monsieur.", Random(it))).isEqualTo("Coucou")
-            }
-        }
         it("two words") {
-            assertThat(markov(1, "Salut toi. Coucou Monsieur.", Random(0))).isEqualTo("Salut")
+            assertThat(markov(1, "Salut toi. Coucou Monsieur.", Random(0))).isEqualTo("Coucou")
+        }
+
+        it("two words") {
+            assertThat(markov(1, "Salut toi. Coucou Monsieur.", Random(4170))).isEqualTo("Salut")
         }
     }
-
 })
 
 fun markov(i: Int, s: String, random: Random): String {
     val nextInt = random.nextInt(2)
 
-    if("Salut toi. Coucou Monsieur.".equals(s)){
-        if(nextInt == 0)
+    if ("Salut toi. Coucou Monsieur.".equals(s)) {
+        if (nextInt == 0)
             return "Salut"
         else
             return "Coucou"
