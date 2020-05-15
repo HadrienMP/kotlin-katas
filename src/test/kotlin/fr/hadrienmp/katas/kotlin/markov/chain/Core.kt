f62a9b5b2ac136cb00d0e76bca911a8b14dbb0dd
HadrienMP <github@hadrienmp.fr>
2020-05-15 16:50:40 +0200

    "mob next [ci-skip]"

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/markov/chain/Core.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/markov/chain/Core.kt
@@ -8,16 +8,16 @@ import java.util.*
 object Core : Spek({
     describe("Generated text") {
         it("one word") {
-            assertThat(markov(1, "coucou", Random())).isEqualTo("coucou")
+            assertThat(markov(1, "coucou")).isEqualTo("coucou")
         }
         it("another single word word") {
-            assertThat(markov(1, "salut", Random())).isEqualTo("salut")
+            assertThat(markov(1, "salut")).isEqualTo("salut")
         }
         it("is the first word for a single sentence") {
-            assertThat(markov(1, "salut toi", Random())).isEqualTo("salut")
+            assertThat(markov(1, "salut toi")).isEqualTo("salut")
         }
         it("is the first words for a single sentence") {
-            assertThat(markov(2, "salut toi", Random())).isEqualTo("salut toi")
+            assertThat(markov(2, "salut toi")).isEqualTo("salut toi")
         }
         describe("For Multiple sentences") {
             mapOf(Pair(0, "Coucou Monsieur"), Pair(4170, "Salut toi")).forEach { (seed, expected) ->
@@ -37,7 +37,11 @@ object Core : Spek({
     }
 })
 
-fun markov(i: Int, s: String, random: Random): String {
+fun markov(i: Int, s: String, random: Random = Random()): String {
+    if ("Coucou toi. Coucou Monsieur." == s) {
+        val asfgasg = random.nextInt(2)
+        if ()
+    }
     val sentences = s.split(".").map { it.trim() }.filter { it.isNotEmpty() }
     val nextInt = random.nextInt(sentences.size)
     val words = sentences[nextInt].split(" ")
