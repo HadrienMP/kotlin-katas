a9cac785b36fc3777704fb728967d21b0d5c38b4
HadrienMP <github@hadrienmp.fr>
2020-05-15 15:30:47 +0200

    "mob next [ci-skip]"

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/markov/chain/Core.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/markov/chain/Core.kt
@@ -9,10 +9,16 @@ object Core : Spek({
         it("one word") {
             assertThat(markov(1, "coucou")).isEqualTo("coucou")
         }
+        it("another single word word") {
+            assertThat(markov(1, "salut")).isEqualTo("salut")
+        }
+        it("two words") {
+            assertThat(markov(1, "salut toi")).isEqualTo("salut")
+        }
     }
 
 })
 
 fun markov(i: Int, s: String): String {
-    return "coucou"
+    return s
 }
