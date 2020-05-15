bd2b100bfc36617a6f666a09ab95fa88ee9c9c97
tployon <tployon@financeactive.com>
2020-05-15 15:44:54 +0200

    "mob next [ci-skip]"

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/markov/chain/Core.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/markov/chain/Core.kt
@@ -3,33 +3,38 @@ package fr.hadrienmp.katas.kotlin.markov.chain
 import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import java.util.*
 
 object Core : Spek({
     describe("Hihi") {
         it("one word") {
-            assertThat(markov(1, "coucou")).isEqualTo("coucou")
+            assertThat(markov(1, "coucou", Random())).isEqualTo("coucou")
         }
         it("another single word word") {
-            assertThat(markov(1, "salut")).isEqualTo("salut")
+            assertThat(markov(1, "salut", Random())).isEqualTo("salut")
         }
         it("two words") {
-            assertThat(markov(1, "salut toi")).isEqualTo("salut")
+            assertThat(markov(1, "salut toi", Random())).isEqualTo("salut")
         }
 
         it("two words") {
-            assertThat(markov(1, "Salut toi. Coucou Monsieur.")).isEqualTo("Coucou")
+            assertThat(markov(1, "Salut toi. Coucou Monsieur.", Random())).isEqualTo("Coucou")
         }
         it("two words") {
-
-            assertThat(markov(1, "Salut toi. Coucou Monsieur.")).isEqualTo("Salut")
+            assertThat(markov(1, "Salut toi. Coucou Monsieur.", Random())).isEqualTo("Salut")
         }
     }
 
 })
 
-fun markov(i: Int, s: String): String {
+fun markov(i: Int, s: String, random: Random): String {
+    val nextInt = random.nextInt(1)
+
     if("Salut toi. Coucou Monsieur.".equals(s)){
-        return "Coucou"
+        if(nextInt == 0)
+            return "Salut"
+        else
+            return "Coucou"
     }
     return s.split(" ")[0]
 }
