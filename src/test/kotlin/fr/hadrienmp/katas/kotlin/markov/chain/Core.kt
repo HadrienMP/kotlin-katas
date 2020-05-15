80e108df4f2581a5c9db39b870ba813ae456115e
HadrienMP <github@hadrienmp.fr>
2020-05-15 15:04:43 +0200

    "mob next [ci-skip]"

--- src/test/kotlin/fr/hadrienmp/katas/kotlin/markov/chain/Core.kt
+++ src/test/kotlin/fr/hadrienmp/katas/kotlin/markov/chain/Core.kt
@@ -1,8 +1,14 @@
 package fr.hadrienmp.katas.kotlin.markov.chain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 
 object Core : Spek({
-
+    describe("Hihi") {
+        it("jaja") {
+            assertThat(true).isTrue()
+        }
+    }
 
 })
